654498bb086afa61a7225ee72ac670cb76439c5a
NelloRossi97 <rossianiello97@gmail.com>
2023-03-10 18:23:43 +0100

    added btn play and canc

--- js/script.js
+++ js/script.js
@@ -4,9 +4,11 @@ Scrivi un programma che stampi in console i numeri da 1 a 100,
 ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
 Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”. 
 */
+const btnCanc = document.getElementById('btnCanc');
+const btnPlay = document.getElementById('btnPlay');
 
-
-for (let i = 1; i <= 100; i++ ){
+btnPlay.addEventListener('click', function (){
+    for (let i = 1; i <= 100; i++ ){
     if ((i % 3 === 0) && (i % 5 === 0)){
         const fizzbuzz = document.getElementById('single-card').innerHTML +=
         `
@@ -29,3 +31,9 @@ for (let i = 1; i <= 100; i++ ){
         `;
     }
 }
+})
+
+
+btnCanc.addEventListener('click', function(){
+    document.getElementById('single-card').innerHTML = "";
+})
