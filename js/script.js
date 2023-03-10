137548df266546d2e4a36b0c16a54030459d5719
NelloRossi97 <rossianiello97@gmail.com>
2023-03-10 15:16:09 +0100

    fixed square position

--- js/script.js
+++ js/script.js
@@ -8,22 +8,22 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
 for (let i = 1; i <= 100; i++ ){
     if ((i % 3 === 0) && (i % 5 === 0)){
-        const fizzbuzz = document.getElementById('cards-container').innerHTML +=
+        const fizzbuzz = document.getElementById('single-card').innerHTML +=
         `
         <div class="square square-fizzbuzz fw-bold">fizzbuzz</div>
         `;
     } else if (i % 5 === 0){
-        const buzz = document.getElementById('cards-container').innerHTML +=
+        const buzz = document.getElementById('single-card').innerHTML +=
         `
         <div class="square square-buzz fw-bold">buzz</div>
         `;
     } else if (i % 3 === 0){
-        const fizz = document.getElementById('cards-container').innerHTML += 
+        const fizz = document.getElementById('single-card').innerHTML += 
         `
         <div class="square square-fizz fw-bold">fizz</div> 
         `;
     } else {
-        const number = document.getElementById('cards-container').innerHTML += 
+        const number = document.getElementById('single-card').innerHTML += 
         `
         <div class="square square-number fw-bold"> ${i}</div>
         `;
