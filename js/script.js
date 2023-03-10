cb95547dc1784210d4d73ea9f57551f41c08eff0
NelloRossi97 <rossianiello97@gmail.com>
2023-03-10 14:17:55 +0100

    added initial structure of cards

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/* 
+square number
+<div class="square square-number fw-bold">1</div>
+
+square fizz
+<div class="square square-fizz fw-bold">fizz</div>  
+
+square buzz
+<div class="square square-buzz fw-bold">buzz</div>
+
+square fizzbuzz
+<div class="square square-fizzbuzz fw-bold">fizzbuzz</div>
+*/
